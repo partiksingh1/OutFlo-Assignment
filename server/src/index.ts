fdf67c0a0c4a5b9b24f2bebcd0bd9b4d5a9dff70
partiksingh1 <20btrcs105@jainuniversity.ac.in>
2025-04-20 15:40:49 +0200

    keep alive funciton

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,7 @@ import campaignRoutes from './routes/campaignRoutes';
 import messageRoutes from './routes/messageRoutes';
 import linkedInProfileRoutes from './routes/scrapingRoutes'
 import scrapingRoutes from './routes/scrapingRoutes'
+import axios from 'axios';
 const PORT = process.env.PORT || 3000;
 dotenv.config();
 
@@ -28,5 +29,20 @@ app.use('/api/v1/scrape', scrapingRoutes);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+const keepServerAlive = () => {
+  const url = 'https://outflo-assignment-ikng.onrender.com';
+
+  setInterval(async () => {
+    try {
+      await axios.get(url);
+      console.log(`Pinged the server at ${url} successfully.`);
+    } catch (error) {
+      console.error(`Failed to ping the server at ${url}:`);
+    }
+  }, 60000); // 60000 ms = 1 minute
+};
+
+// Start the keep-alive function
+keepServerAlive();
 
 export default app;
